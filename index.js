afac130c78dfa13da44cc548e8e2025e0a6a8156
Raphael Melo <contatowbm@gmail.com>
2021-03-03 23:48:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+function capturar() {
+    let nome = document.getElementById('inputNome').value;
+    let Snome = document.getElementById('inputSNome').value;
+    document.getElementById('valorDigitado').innerHTML = nome + "  " + Snome;
+
+}
+
